ed599a6002c1f2b39cdd2996c52303223cae7106
ReXeR00 <106077454+ReXeR00@users.noreply.github.com>
2025-11-06 18:20:50 +0100

    Rename base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/matching-pairs/",
-})  
+  base: "https://<ReXeR00>.github.io/<matching-pairs>/",
+});
